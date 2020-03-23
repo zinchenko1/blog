d0331c3ed3ce8a6aa33c9fb9ab57643bf922afd9
zrayev <dmitryzrayev@gmail.com>
2020-03-23 09:55:05 +0200

    installed FOSElasticaBundle

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
