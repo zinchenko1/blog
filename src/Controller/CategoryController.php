3f79a2afc2a6d75fd89ee50ec536b509b5e4ab48
zinchenko1 <aswad1909@gmail.com>
2020-03-30 23:46:22 +0300

    add pagination post && style pagination && output all category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,6 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
+     * @Route("/category", name="category_index")
+     * @return Response
+     */
+    public function getCategories(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repository->findAll();
+
+        return $this->render('/category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+
+    }
+
+    /**
      * @Route("/category/{categorySlug}", name="category_posts", methods={"GET"})
      * @return Response
      * @ParamConverter("category", options={"mapping" : {"categorySlug" : "slug"}})
